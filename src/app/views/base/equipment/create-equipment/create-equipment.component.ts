a738562b263f644d7b4418f5659a14c475acff77
kudakwashe <kuda@nightsbridge.co.za>
2021-02-14 13:06:27 +0200

    Added equiment component

---
+++ src/app/views/base/equipment/create-equipment/create-equipment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-equipment',
+  templateUrl: './create-equipment.component.html',
+  styleUrls: ['./create-equipment.component.scss']
+})
+export class CreateEquipmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
